79d69d3a33596b08ecd0ba9b1b6b87da80bc53da
Geshh <gesha95_gesha95@abv.bg>
2017-01-19 18:55:11 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Super fast drone delivery system!");
-		System.out.println("ALOOOO");
-		System.out.println("BOLOC");
 	}
 
 }
