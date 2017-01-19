8bb967d6ff6b6349c958d24dbaeba0f4a81e78c8
Boris Altanov <borisaltanov@gmail.com>
2017-01-19 18:44:31 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Super fast drone delivery system!");
 		System.out.println("ALOOOO");
+		System.out.println("BOLOC");
 	}
 
 }
