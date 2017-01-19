f91cf38a22739e81336d2f88e324d476e03764dd
Boris Altanov <borisaltanov@gmail.com>
2017-01-19 20:39:31 +0200

    droneManager canDeliver

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
+		DroneManager dm = new DroneManager();
+		dm.drones.get(2).setIsFlying(true);
+		System.out.println(dm.canDeliver(900, 1499));
+		System.out.println("Super fast drone delivery system!");
+
+		Scanner input = new Scanner(System.in);
+
+		String req = input.nextLine();
+
+		Request request = new Request(req);
 
 		Scanner input = new Scanner(System.in);
 
