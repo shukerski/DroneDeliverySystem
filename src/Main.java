c6622debd58bc54eef2e85b5d4c0db7732202123
Geshh <gesha95_gesha95@abv.bg>
2017-01-19 19:30:59 +0200

    req parsing

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Super fast drone delivery system!");
+
+		Scanner input = new Scanner(System.in);
+
+		String req = input.nextLine();
+
+		Request request = new Request(req);
 	}
 
 }
---
+++ src/Request.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+
+public class Request {
+
+	private int id;
+	private int reqX;
+	private int reqY;
+	private Map<Integer,Integer> productsToDeliver;
+	
+	public Request(String request) {
+		
+	}
+	
+}
