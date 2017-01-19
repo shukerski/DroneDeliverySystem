565bf605f85775a664885e4c3e488703db9f575b
Boris Altanov <borisaltanov@gmail.com>
2017-01-19 19:53:25 +0200

    droneManager canDeliver

--- src/drone/DroneManager.java
+++ src/drone/DroneManager.java
@@ -2,19 +2,33 @@ package drone;
 import java.util.ArrayList;
 
 public class DroneManager {
-	private ArrayList<DefaultDrone> drones;
+	public ArrayList<DefaultDrone> drones = new ArrayList<>();
 	private int numberOfDrones = 50;
 	
 	
 	public DroneManager() {
 		for (int i = 0; i < numberOfDrones; i++) {
-			drones.add(new DefaultDrone(i));
+			drones.add(new DefaultDrone(i + 1));
 		}
 		
 	}
 	
-	
-	
+	public boolean canDeliver(int distance, double weight) {
+		String message = "Using drones: ";
+		for (DefaultDrone drone : drones) {
+			
+			if (drone.getIsFlying() == false && drone.getBattery() >= distance * 2) {
+				message += drone.getId() + ", ";
+				weight -= drone.getCapacity();
+			}
+			
+			if (weight < 0) {
+				System.out.println(message);
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,14 @@
 package main;
 import java.util.Scanner;
 
+import drone.DroneManager;
+
 public class Main {
 
 	public static void main(String[] args) {
+		DroneManager dm = new DroneManager();
+		dm.drones.get(2).setIsFlying(true);
+		System.out.println(dm.canDeliver(900, 1200));
 		System.out.println("Super fast drone delivery system!");
 
 		Scanner input = new Scanner(System.in);
@@ -11,6 +16,8 @@ public class Main {
 		String req = input.nextLine();
 
 		Request request = new Request(req);
+		
 	}
+		
 
 }
