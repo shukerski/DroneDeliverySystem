72c6cdffa55d108ad8b77f25fc629bd8d3900de0
Boris Altanov <borisaltanov@gmail.com>
2017-01-24 20:38:33 +0200

    chinese drone

---
+++ src/drone/ChineseDrone.java
@@ -0,0 +1,11 @@
+package drone;
+
+public class ChineseDrone extends Drone {
+
+	public ChineseDrone(int id) {
+		super(id, 1200, 200, 3, false);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
