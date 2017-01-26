65f1c241eb10990fb944a5a5d007b945e5d7cf68
Geshh <gesha95_gesha95@abv.bg>
2017-01-26 20:26:11 +0200

    dd

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,36 +10,37 @@ import requests.RequestManager;
 import warehouse.Warehouse;
 import warehouse.WarehouseManager;
 
-
 public class Main {
-	
+
 	static String req = "delivery 4 2016-10-25 12:31 420,369 1 5 2 20";
-	
+
 	public static void main(String[] args) {
-		
+
 		Warehouse w = new Warehouse(42, 42);
 		w.addProduct(new Product(1, "Waffle", 1), 20);
 		w.addProduct(new Product(2, "Cola", 2), 50);
 
-		Warehouse w2 = new Warehouse(420,420);
+		Warehouse w2 = new Warehouse(420, 420);
 		w2.addProduct(new Product(1, "Waffle", 1), 20);
 		w2.addProduct(new Product(2, "Cola", 2), 50);
-		
+
 		ArrayList<Warehouse> warehouses = new ArrayList<>();
 		warehouses.add(w);
 		warehouses.add(w2);
-		
+
 		WarehouseManager wm = new WarehouseManager(warehouses);
 		DroneManager dm = new DroneManager();
-		
+		RequestManager rm = new RequestManager();
 
-		Request request = InputParser.createRequest(req);
+		Request req1 = InputParser.createRequest(req);
+		Request req2 = InputParser.createRequest(req);
+		rm.addRequest(req1);
+		rm.addRequest(req2);
 
-		
-		System.out.println(RequestManager.canExecuteRequest(request, wm, dm));
+		System.out.println(rm.canExecuteRequest(wm, dm));
 
 		LocalDate date = LocalDate.now();
 		System.out.println(date);
-	
+
 	}
 }
--- src/requests/RequestManager.java
+++ src/requests/RequestManager.java
@@ -1,24 +1,44 @@
 package requests;
 
+import java.util.Queue;
+
 import drone.DroneManager;
 import warehouse.Warehouse;
 import warehouse.WarehouseManager;
 
 public class RequestManager {
-	public static boolean canExecuteRequest(Request request, WarehouseManager wm, DroneManager dm) {
+
+	private Queue<Request> requests;
+
+	public RequestManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void addRequest(Request r) {
+		requests.add(r);
+	}
+
+	public boolean hasRequests() {
+		return requests.isEmpty();
+	}
+
+	public boolean canExecuteRequest(WarehouseManager wm, DroneManager dm) {
+
+		Request request = requests.poll();
+
 		Warehouse w = wm.checkWarehouses(request);
 		double weight = 0, distance = 0;
 		boolean canExecute = false;
-		if(w != null) {
+		if (w != null) {
 			distance = wm.calculateDistance(w, request.getX(), request.getY());
 			weight = wm.calculateTotalWeight(w, request.getProductsToDeliver());
 			canExecute = dm.canDeliver(distance, weight);
-			if(canExecute) {
+			if (canExecute) {
 				w.update(request);
 			}
 		}
+
 		return canExecute;
 	}
-		
-	
+
 }
