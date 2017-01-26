19fc1150344d346246ff0d7e110efec94261b6f6
Geshh <gesha95_gesha95@abv.bg>
2017-01-26 19:37:14 +0200

    speed is the most important thing in life

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,25 +13,28 @@ import warehouse.WarehouseManager;
 
 public class Main {
 	
+	static String req = "delivery 4 2016-10-25 12:31 420,369 1 5 2 20";
+	
 	public static void main(String[] args) {
-		DroneManager dm = new DroneManager();
-		dm.drones.get(2).setIsFlying(true);
-
-		Scanner input = new Scanner(System.in);
-
-		String req = "delivery 4 2016-10-25 12:31 420,369 1 5 2 20";
-
-		Request request = InputParser.createRequest(req);
 		
 		Warehouse w = new Warehouse(42, 42);
-		Warehouse w2 = new Warehouse(420, 420);
+		w.addProduct(new Product(1, "Waffle", 1), 20);
+		w.addProduct(new Product(2, "Cola", 2), 50);
+
+		Warehouse w2 = new Warehouse(420,420);
+		w2.addProduct(new Product(1, "Waffle", 1), 20);
+		w2.addProduct(new Product(2, "Cola", 2), 50);
+		
 		ArrayList<Warehouse> warehouses = new ArrayList<>();
 		warehouses.add(w);
 		warehouses.add(w2);
-		w.addProduct(new Product(1, "Waffle", 1), 20);
-		w2.addProduct(new Product(2, "Cola", 2), 50);
 		
 		WarehouseManager wm = new WarehouseManager(warehouses);
+		DroneManager dm = new DroneManager();
+		
+
+		Request request = InputParser.createRequest(req);
+
 		
 		
 		System.out.println(RequestManager.canExecuteRequest(request, wm, dm));
